1414e1fe496111b450aa11b85814f2b38e25793e
matree <1060778075@qq.com>
2019-10-29 17:19:35 +0800

    gitignore

---
+++ wmall-item-web/src/test/java/com/matree/wmall/item/WmallItemWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matree.wmall.item;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmallItemWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
